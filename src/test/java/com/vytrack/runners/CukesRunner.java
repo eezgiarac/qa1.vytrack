f166d0e834aa20dc262ea754cd051a56691a4307
eezgiarac <eezgiarac@gmail.com>
2024-12-23 11:21:57 +0100

    CukesRunner and features are updated

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -1,4 +1,4 @@
-package java.com.vytrack.runners;
+package com.vytrack.runners;
 
 
 import io.cucumber.junit.Cucumber;
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@test05",
+        tags = "@login",
         publish = true //generating a report with public link
 )
 public class CukesRunner {
